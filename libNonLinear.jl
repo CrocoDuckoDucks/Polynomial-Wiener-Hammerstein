c6238ebbcb076704bd08a620015cd62ebc21029c
CrocoDuck <CrocoDuckoDucks@users.noreply.github.com>
2019-02-14 22:04:08 +0000

    Bug fixes, removed unnecessary dependency on Fs, more validation code

--- libNonLinear.jl
+++ libNonLinear.jl
@@ -19,7 +19,7 @@ function wrapTo2Pi(x::AbstractArray)
     return wrapTo2Pi!(copy(x))
 end
 
-function fcircshift(x::AbstractArray, s::Real)
+function fcircshiftExp(x::AbstractArray, s::Real)
 
     N = length(x)
     nyq = div(N, 2) + 1
@@ -31,11 +31,15 @@ function fcircshift(x::AbstractArray, s::Real)
     phFactor[1:nyq] = exp.(-im .* ph)
     phFactor[mod.(N .+ 1 .- (1:nyq), N) .+ 1]  = conj.(phFactor[1:nyq])
 
-    return ifft(fft(x) .* phFactor)
+    return phFactor
 
 end
 
-function fcircshift(x::AbstractArray{<:Real}, s::Real)
+function fcircshift(x::AbstractArray, s::Real)
+    return ifft(fft(x) .* fcircshiftExp(x, s))
+end
+
+function fcircshiftExp(x::AbstractArray{<:Real}, s::Real)
 
     N = length(x)
     nyq = div(N, 2) + 1
@@ -46,8 +50,12 @@ function fcircshift(x::AbstractArray{<:Real}, s::Real)
     phFactor = ones(Complex, nyq)
     phFactor = exp.(-im .* ph)
 
-    return irfft(rfft(x) .* phFactor, N)
+    return phFactor
 
+end
+
+function fcircshift(x::AbstractArray{<:Real}, s::Real)
+    return irfft(rfft(x) .* fcircshiftExp(x, s), N)
 end
 
 function cheb(order::Integer, x::AbstractArray{<:Real})
@@ -113,21 +121,11 @@ end
 function branchAntialFirTaps(
     nBranch::Integer,
     nTaps::Integer,
-    ripple::Real,
-    Fs::Real
+    ripple::Real
     )
 
-    if nBranch == 1
-
-        h = zeros(nTaps)
-        h[div(nTaps, 2) + 1] = 1.0
-
-        return h
-
-    end
-
-    rType = Lowpass(Fs / 2nBranch, fs = Fs)
-    dMeth = FIRWindow(chebwin(nTaps, ripple))
+    rType = Lowpass((nTaps - 1) / (nBranch * nTaps))
+    dMeth = FIRWindow(hanning(nTaps)) #FIRWindow(chebwin(nTaps, ripple))
 
     return digitalfilter(rType, dMeth)
 
@@ -137,10 +135,10 @@ function randomUniform(a::Real, b::Real)
     return (b - a) * rand() + a
 end
 
-function randomFrequency(Fs::Real, fMin::Real = 20.0)
+function randomFrequency(fMin::Real = 1e-4)
 
     a = log10(fMin)
-    b = log10(0.5 * Fs)
+    b = log10(1.0 - eps(1.0))
 
     return exp10(randomUniform(a, b))
 
@@ -154,31 +152,31 @@ function randomLinGain(minGain_dB::Real, maxGain_dB::Real)
     return dB2lin(randomdBGain(minGain_dB, maxGain_dB))
 end
 
-function randomLowpass(Fs::Real, fMin::Real)
-    return Lowpass(randomFrequency(Fs, fMin), fs = Fs)
+function randomLowpass(fMin::Real)
+    return Lowpass(randomFrequency(fMin))
 end
 
-function randomHighpass(Fs::Real, fMin::Real)
-    return Highpass(randomFrequency(Fs, fMin), fs = Fs)
+function randomHighpass(fMin::Real)
+    return Highpass(randomFrequency(fMin))
 end
 
-function randomBandpass(Fs::Real, fMin::Real)
+function randomBandpass(fMin::Real)
 
-    corners = sort([randomFrequency(Fs, fMin); randomFrequency(Fs, fMin)])
+    corners = sort([randomFrequency(fMin); randomFrequency(fMin)])
 
-    return Bandpass(corners[1], corners[2], fs = Fs)
-
-end
-
-function randomBandstop(Fs::Real, fMin::Real)
-
-    corners = sort([randomFrequency(Fs, fMin); randomFrequency(Fs, fMin)])
-
-    return Bandstop(corners[1], corners[2], fs = Fs)
+    return Bandpass(corners[1], corners[2])
 
 end
 
-function randomResponseType(Fs::Real, fMin::Real)
+function randomBandstop(fMin::Real)
+
+    corners = sort([randomFrequency(fMin); randomFrequency(fMin)])
+
+    return Bandstop(corners[1], corners[2])
+
+end
+
+function randomResponseType(fMin::Real)
 
     dTypes = [
         randomLowpass;
@@ -187,7 +185,7 @@ function randomResponseType(Fs::Real, fMin::Real)
         randomBandstop
         ]
 
-    return dTypes[rand(1:end)](Fs, fMin)
+    return dTypes[rand(1:end)](fMin)
 
 end
 
@@ -263,7 +261,6 @@ function randomDesignMethod(
 end
 
 function randomIIR(
-    Fs::Real,
     fMin::Real,
     maxOrder::Integer,
     rippleLim1::Real,
@@ -271,13 +268,14 @@ function randomIIR(
     )
 
     return digitalfilter(
-        randomResponseType(Fs, fMin),
+        randomResponseType(fMin),
         randomDesignMethod(maxOrder, rippleLim1, rippleLim2)
         )
 
 end
 
 struct pwh
+    N::Integer
     antiAls
     kernels
     gains
@@ -285,7 +283,6 @@ end
 
 function randomPWH(
     N::Integer,
-    Fs::Real,
     nTaps::Integer,
     ripple::Real,
     fMin::Real,
@@ -302,17 +299,21 @@ function randomPWH(
 
     for n in 1:N
 
-        append!(antiAls, [branchAntialFirTaps(n, nTaps, ripple, Fs)])
-        append!(kernels, [randomIIR(Fs, fMin, maxOrder, rippleLim1, rippleLim2)])
+        append!(antiAls, [branchAntialFirTaps(n, nTaps, ripple)])
+        append!(kernels, [randomIIR(fMin, maxOrder, rippleLim1, rippleLim2)])
         append!(gains, randomLinGain(minGain_dB, maxGain_dB))
 
     end
 
-    return pwh(antiAls, kernels, gains)
+    return pwh(N, antiAls, kernels, gains)
 
 end
 
-function writeSos(fID::IOStream, sos::SecondOrderSections, biqFcn::String = "tf2np")
+function writeSos(
+    fID::IOStream,
+    sos::SecondOrderSections,
+    biqFcn::String = "tf2np"
+    )
 
     write(fID, "*(_, $(sos.g)) : ")
 
@@ -336,28 +337,20 @@ function pwh2faust(pwhObj::pwh, dspPath::String, biqFcn::String)
 
         write(fID, """fi = library("filters.lib");\nde = library("delays.lib");\n\n""")
 
-        for n in 1:length(pwhObj.antiAls)
+        for n in 1:pwhObj.N
 
             sos = convert(SecondOrderSections, pwhObj.kernels[n])
 
-            if n == 1
+            write(fID, "br$n = fi.fir((")
 
-                write(fID, "br1 = de.fdelay($(ceil(Integer, 0.5 * length(a.antiAls[1]))), $(0.5 * (length(a.antiAls[1]) - 1))) : ")
+            for t in 1:length(pwhObj.antiAls[n])
 
-            else
+                write(fID, "$(pwhObj.antiAls[n][t])")
 
-                write(fID, "br$n = fi.fir((")
-
-                for t in 1:length(pwhObj.antiAls[n])
-
-                    write(fID, "$(pwhObj.antiAls[n][t])")
-
-                    if t == length(pwhObj.antiAls[n])
-                        write(fID, ")) : pow(_, $(n)) : *(_, $(pwhObj.gains[n])) : ")
-                    else
-                        write(fID, ", ")
-                    end
-
+                if t == length(pwhObj.antiAls[n])
+                    write(fID, ")) : pow(_, $(n)) : *(_, $(pwhObj.gains[n])) : ")
+                else
+                    write(fID, ", ")
                 end
 
             end
@@ -370,12 +363,12 @@ function pwh2faust(pwhObj::pwh, dspPath::String, biqFcn::String)
 
         write(fID, "\nprocess = _ <: ")
 
-        for n in 1:length(pwhObj.antiAls)
+        for n in 1:pwhObj.N
 
             write(fID, "br$(n)")
 
             if n == length(pwhObj.antiAls)
-                write(fID, " :> fi.dcblockerat(10);\n")
+                write(fID, " :> _;\n")
             else
                 write(fID, ", ")
             end
@@ -388,12 +381,13 @@ end
 
 function pwh2plot(pwhObj::pwh, wLength::Integer, Fs::Real)
 
-    plt = plot(layout = 4, xlabel = "Frequency [Hz]")
+    plt = plot(layout = 4)
 
     plot!(
         plt,
         ylabel = "Magnitude [dB]",
         title = "Branch Antialiasing",
+        xlabel = "Frequency [half-cycles/sample]",
         subplot = 1
         )
 
@@ -401,6 +395,7 @@ function pwh2plot(pwhObj::pwh, wLength::Integer, Fs::Real)
         plt,
         ylabel = "Angle [rad]",
         title = "Branch Antialiasing",
+        xlabel = "Frequency [half-cycles/sample]",
         subplot = 3
         )
 
@@ -408,6 +403,7 @@ function pwh2plot(pwhObj::pwh, wLength::Integer, Fs::Real)
         plt,
         ylabel = "Magnitude [dB]",
         title = "Branch Kernel",
+        xlabel = "Frequency [Hz]",
         subplot = 2
         )
 
@@ -415,23 +411,22 @@ function pwh2plot(pwhObj::pwh, wLength::Integer, Fs::Real)
         plt,
         ylabel = "Angle [rad]",
         title = "Branch Kernel",
+        xlabel = "Frequency [Hz]",
         subplot = 4
         )
 
     fKernels = (0:(wLength - 1)) * Fs / 2wLength
 
-    for n in 1:length(pwhObj.antiAls)
+    for n in 1:pwhObj.N
 
         H = rfft(pwhObj.antiAls[n])
 
-        fAntial = (0:(length(H) - 1)) *
-            Fs / 2length(H)
+        fAntial = (0:(length(H) - 1)) / length(H)
 
         plot!(
             plt,
             fAntial[2:end],
             lin2dB.(H[2:end]),
-            xscale = :log10,
             label = "$n",
             subplot = 1
             )
@@ -440,7 +435,6 @@ function pwh2plot(pwhObj::pwh, wLength::Integer, Fs::Real)
             plt,
             fAntial[2:end],
             unwrap(angle.(H[2:end])),
-            xscale = :log10,
             label = "$n",
             subplot = 3
             )
@@ -471,11 +465,181 @@ function pwh2plot(pwhObj::pwh, wLength::Integer, Fs::Real)
 
 end
 
+function pwh2matrix(pwhObj::pwh, wLength::Integer, Fs::Real)
+
+    G           = zeros(Complex{Float64}, length(pwhObj.antiAls), wLength)
+    fKernels    = (0:(wLength - 1)) * Fs / 2wLength
+
+    for n in 1:length(pwhObj.antiAls)
+        G[n, :] = pwhObj.gains[n] .* freqz(pwhObj.kernels[n], fKernels, Fs)
+    end
+
+    return G, fKernels
+
+end
+
 function readMatrixCsv(rePath::String, imPath::String)
 
     Mre = convert(Array{Complex{Float64}, 2}, CSV.read("$rePath", header = 0))
     Mim = im * convert(Array{Float64, 2}, CSV.read("$imPath", header = 0))
 
     return  Mre + Mim
+
+end
+
+function matrix2plot(M::AbstractArray, Fs::Real)
+
+    nyq = div(size(M, 2), 2) + 1
+
+    f = (0:(nyq - 1)) * Fs / 2nyq
+
+    plt = plot(layout = 2, xlabel = "Frequency [Hz]")
+
+    plot!(
+        plt,
+        f[2:end],
+        transpose(lin2dB.(M[:, 2:nyq])),
+        ylabel = "Magnitude [dB]",
+        xscale = :log10,
+        subplot = 1
+        )
+
+    plot!(
+        plt,
+        f[2:end],
+        unwrap(transpose(angle.(M[:, 2:nyq])), dims = 1),
+        ylabel = "Angle [rad]",
+        xscale = :log10,
+        subplot = 2
+        )
+
+    return plt
+
+end
+
+function cMatrix(N::Integer, A::Real = 1.0)
+
+    C = zeros(Complex, N, N)
+
+    for r in 1:N
+
+        a = A^(r - 1)
+
+        for c in r:N
+
+            if !iseven(r + c)
+                continue
+            end
+
+            C[r, c] =
+                a *
+                Complex(-1)^(2c + (1 - r) / 2) *
+                exp2(1 - c) *
+                binomial(c, div(c - r, 2))
+
+        end
+
+    end
+
+    return C
+
+end
+
+function novakWin(M::Integer, I::Integer, O::Integer)
+
+    @assert I <= (M - O + 1)
+
+    w = ones(M)
+
+    w[1:(I - 1)] =
+        0.5 * sinpi.((2((1:(I - 1)) .- 1) .- I .+ 1) ./ (2(I - 1))) .+ 0.5
+
+
+    w[end:-1:(end - O + 2)] =
+        0.5 * sinpi.((2((1:(O - 1)) .- 1) .- O .+ 1) ./ (2(O - 1))) .+ 0.5
+
+    return w
+
+end
+
+function higherGap(γ::Real, n::Integer, Fs::Real)
+    return Fs * γ * log(n)
+end
+
+function gap2next(γ::Real, n::Integer, Fs::Real)
+    return Fs * γ * log(1.0 + 1.0 / n)
+end
+
+function gap2previous(γ::Real, n::Integer, Fs::Real)
+    Fs * γ * log(n / (n - 1))
+end
+
+function windowHighers(
+    h::AbstractArray,
+    M::Integer,
+    N::Integer,
+    γ::Real,
+    Fs::Real,
+    innerWin,
+    outerWin
+    )
+
+    c = div(length(h), 2)
+
+    H = zeros(Complex{Float64}, N, M)
+
+    for n in 1:N
+
+        w               = zeros(M)
+
+        fCenter         = c - higherGap(γ, n, Fs)
+        fGap2Next       = gap2next(γ, n, Fs)
+        fGap2Previous   = gap2previous(γ, n, Fs)
+
+        fOptHead        = fCenter - min(0.5 * fGap2Next, 0.5 * M)
+        fOptTail        = fCenter + min(0.5 * fGap2Previous, 0.5 * M)
+        fOptLength      = fOptTail - fOptHead
+        nMaxData        = floor(Integer, fOptLength)
+
+        nOptHead        = floor(Integer, fOptHead)
+        nOptTail        = floor(Integer, fOptTail)
+        fOptCount       = floor(Integer, fOptLength)
+
+        fHead           = fOptHead
+        nHead           = nOptHead
+
+        fWhead          = 0.5 * (M - min(fGap2Next, M)) + 1
+        nWhead          = floor(Integer, fWhead)
+
+        cShift          = nHead - fHead
+        wShift          = fWhead - nWhead
+
+        alignShift      = cShift + wShift
+        totalShift      = alignShift + 0.5 * M
+
+        w[nWhead:(nWhead + nMaxData - 1)] = h[nHead:(nHead + nMaxData - 1)] .*
+            innerWin(nMaxData)
+
+        H[n, :] = fft(w .* outerWin(M))
+        H[n, :] .*= fcircshiftExp(H[n, :], totalShift)
+
+    end
+
+    return H
+
+end
+
+function hammSolve(
+    h::AbstractArray,
+    M::Integer,
+    N::Integer,
+    γ::Real,
+    A::Real,
+    Fs::Real,
+    innerWin,
+    outerWin
+    )
+
+    return cMatrix(N, A) \ windowHighers(h, M, N, γ, Fs, innerWin, outerWin)
 
 end
